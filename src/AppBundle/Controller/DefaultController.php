c0d87144aca0a77f5db71316e464c7dad2254327
youssef-e <youssef.ellaabi@everycheck.fr>
2021-03-02 11:49:12 +0100

    initial commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        dump($this->get("doctrine"));
+      //  throw new \Exception("Error Processing Request", 1);
+        
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
