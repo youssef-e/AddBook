a2e0247e71e8eb526932f68112e7126173516f14
youssef-e <youssef.ellaabi@everycheck.fr>
2021-03-02 12:06:00 +0100

    added place entity and crud action

---
+++ src/AppBundle/Entity/Place.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Place
+ *
+ * @ORM\Table(name="place")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PlaceRepository")
+ */
+class Place
+{
+    /**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    public $name;
+
+    /**
+     * @ORM\Column(name="address", type="string", length=255)
+     */
+    public $address;
+
+    /**
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    public $createdAt;
+
+    /**
+     * @ORM\Column(name="comment", type="string", length=255)
+     */
+    public $comment;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+}
---
+++ src/AppBundle/Form/PlaceType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlaceType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('address')->add('createdAt')->add('comment');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Place'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_place';
+    }
+
+
+}
