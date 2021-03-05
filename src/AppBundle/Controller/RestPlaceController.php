9e298539fd0188e32ebd7fcac010eb81ee7ce868
youssef-e <youssef.ellaabi@everycheck.fr>
2021-03-05 11:17:21 +0100

    fixed delete place method

--- src/AppBundle/Controller/RestPlaceController.php
+++ src/AppBundle/Controller/RestPlaceController.php
@@ -62,19 +62,6 @@ class RestPlaceController extends Controller
         
     }
 
-    /**
-     * Finds and displays a place entity.
-     *
-     * @Route("/{id}", name="rest_place_show")
-     * @Method("GET")
-     */
-    public function showAction(Place $place)
-    {
-        $response = new Response(json_encode($place));
-        $response->headers->set('Content-Type', 'application/json');
-
-        return $response;
-    }
 
     /**
      * Displays a form to edit an existing place entity.
@@ -83,9 +70,7 @@ class RestPlaceController extends Controller
      * @Method({"GET", "POST"})
      */
     public function editAction(Request $request, Place $place)
-    {
-        $deleteForm = $this->createDeleteForm($place);
-        
+    {   
         if($request->isMethod('get')){
             $response = new Response(json_encode($place));
             $response->headers->set('Content-Type', 'application/json');
@@ -120,36 +105,15 @@ class RestPlaceController extends Controller
      * @Method("DELETE")
      */
     public function deleteAction(Request $request, Place $place)
-    {
-        $form = $this->createDeleteForm($place);
-        $data=json_decode($request->getContent(),true);
-        $form->submit($data);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            dump($em);
-            $em->remove($place);
-            $em->flush();
-        }
-        $response = new Response(json_encode($place));
+    { 
+        $em = $this->getDoctrine()->getManager();
+        dump($em);
+        $em->remove($place);
+        $em->flush();
+        $response = new Response(null,Response::HTTP_NO_CONTENT);
         $response->headers->set('Content-Type', 'application/json');
 
         return $response;
     }
 
-    /**
-     * Creates a form to delete a place entity.
-     *
-     * @param Place $place The place entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createDeleteForm(Place $place)
-    {
-        return $this->createFormBuilder(null,array('csrf_protection'   => false))
-            ->setAction($this->generateUrl('place_delete', array('id' => $place->getId())))
-            ->setMethod('DELETE')
-            ->getForm()
-        ;
-    }
 }
